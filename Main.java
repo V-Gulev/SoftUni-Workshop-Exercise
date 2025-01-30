482d3c188d6616df836360be083741abda11a0a9
V-Gulev <velizargulev@abv.bg>
2025-01-30 20:07:22 +0200

    SmartArray

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        SmartArray smartArray = new SmartArray();
+        smartArray.add(1);
+        smartArray.add(2);
+        smartArray.add(3);
+        smartArray.add(4);
+        smartArray.add(5);
 
+        System.out.println(smartArray.get(3));
     }
 }
